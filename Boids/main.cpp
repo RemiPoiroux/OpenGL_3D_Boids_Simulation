8a2ee2c3c56e9bfedb86867bbd051f8ca0685735
RemiPoiroux <Vous@exemple.com>
2023-05-02 01:26:42 +0200

    Corrected glEnable's Segmentation Error

--- Boids/main.cpp
+++ Boids/main.cpp
@@ -35,21 +35,24 @@ int main()
     /////////////////////////////////
     /////////////////////////////////
 
-    glEnable(GL_DEPTH_TEST);
-
     int  width  = 1280;
     int  height = 720;
     auto ctx    = p6::Context{{width, height, "Remi's Boids"}};
     ctx.maximize_window();
 
+    // Initialize depth test
+    glEnable(GL_DEPTH_TEST);
+
+    // Initialize camera
+    FreeflyCamera camera;
+
+    // Initialize Boids and obstacles
+    std::vector<Boid>     boids     = createBoids(NB_BOIDS);
+    std::vector<Obstacle> obstacles = createObstacles(OBSTACLES_PARAMETERS);
+
     // Initialize Render Matrices
     glm::mat4 ProjMatrix = glm::perspective(glm::radians(viewField), static_cast<GLfloat>(width) / static_cast<GLfloat>(height), 0.01f, 3.f);
     glm::mat4 ViewMatrix;
-
-    FreeflyCamera camera;
-
-    std::vector<Boid>     boids     = createBoids(NB_BOIDS);
-    std::vector<Obstacle> obstacles = createObstacles(OBSTACLES_PARAMETERS);
 
     // Initialize Maps of Buffers
     MyBuffers vbos{};
